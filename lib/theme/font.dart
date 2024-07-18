3b011ba0172c41afd28a93a72a69d0b8ad04a565
haram22 <kimhr9889@handong.ac.kr>
2024-07-18 14:50:32 +0900

    [Fix] font에 컬러 파라이터 추가

--- lib/theme/font.dart
+++ lib/theme/font.dart
@@ -1,64 +1,100 @@
 import 'package:flutter/material.dart';
 
 // Header styles
-const TextStyle header0 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 32.0,
-);
+TextStyle header0([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 32.0,
+    color: color,
+  );
+}
 
-const TextStyle header1R = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 26.0,
-);
+TextStyle header1R([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 26.0,
+    color: color,
+  );
+}
 
-const TextStyle header1B = TextStyle(
-  fontFamily: 'apple_bold',
-  fontSize: 26.0,
-);
+TextStyle header1B([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_bold',
+    fontSize: 26.0,
+    color: color,
+  );
+}
 
-const TextStyle header2 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 20.0,
-);
+TextStyle header2([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 20.0,
+    color: color,
+  );
+}
 
-const TextStyle header3R = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 18.0,
-);
+TextStyle header3R([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 18.0,
+    color: color,
+  );
+}
 
-const TextStyle header3B = TextStyle(
-  fontFamily: 'apple_bold',
-  fontSize: 18.0,
-);
+TextStyle header3B([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_bold',
+    fontSize: 18.0,
+    color: color,
+  );
+}
 
-const TextStyle header4 = TextStyle(
-  fontFamily: 'apple_bold',
-  fontSize: 15.0,
-);
+TextStyle header4([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_bold',
+    fontSize: 15.0,
+    color: color,
+  );
+}
 
 // Body styles
-const TextStyle body1 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 15.0,
-);
+TextStyle body1([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 15.0,
+    color: color,
+  );
+}
 
-const TextStyle body2 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 14.0,
-);
+TextStyle body2([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 14.0,
+    color: color,
+  );
+}
 
-const TextStyle body3 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 13.0,
-);
+TextStyle body3([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 13.0,
+    color: color,
+  );
+}
 
 // Caption styles
-const TextStyle caption1 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 12.0,
-);
+TextStyle caption1([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 12.0,
+    color: color,
+  );
+}
 
-const TextStyle caption2 = TextStyle(
-  fontFamily: 'apple_r',
-  fontSize: 11.0,
-);
+TextStyle caption2([Color color = Colors.black]) {
+  return TextStyle(
+    fontFamily: 'apple_r',
+    fontSize: 11.0,
+    color: color,
+  );
+}
