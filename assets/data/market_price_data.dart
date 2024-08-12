02547530ef0153dc14a144a967d2c249ea91872c
haram22 <kimhr9889@handong.ac.kr>
2024-08-12 16:20:24 +0900

    [Fix] dio 설치

---
+++ assets/data/market_price_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class MarketPriceData {
+  final String date;
+  final String price;
+
+  MarketPriceData({required this.date, required this.price});
+}
