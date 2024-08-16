7d8f03d7c15d95b2ba718b07052433a24dd5656f
haram22 <kimhr9889@handong.ac.kr>
2024-08-17 00:55:24 +0900

    [chore] add 주석

--- lib/home/view/ledger_page.dart
+++ lib/home/view/ledger_page.dart
@@ -183,6 +183,7 @@ class _MarketPriceTableState extends State<MarketPriceTable> {
 
   // 등록된 어종 목록
   final List<String> registeredSpecies = ['문어', '전복', '방어', '소라'];
+  // 등록된 조합명
   final String mxtrNm = '거제수산업협동조합';
 
   Map<String, Map<String, dynamic>> groupedData = {};
