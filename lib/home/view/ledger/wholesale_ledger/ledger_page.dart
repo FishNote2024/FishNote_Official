b40515a3e32556846cf5685eea9a5d077c3ae2aa
haram22 <kimhr9889@handong.ac.kr>
2024-08-19 00:19:49 +0900

    [Feat] bottom sheet를 sliding panel로 교체

--- lib/home/view/ledger/wholesale_ledger/ledger_page.dart
+++ lib/home/view/ledger/wholesale_ledger/ledger_page.dart
@@ -4,7 +4,7 @@ import 'package:fish_note/home/view/ledger/wholesale_ledger/line_chart_view.dart
 import 'package:fish_note/home/view/ledger/wholesale_ledger/pie_chart_view.dart';
 import 'package:fish_note/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 import '../../../../theme/font.dart';
@@ -20,6 +20,7 @@ class _LedgerPageState extends State<LedgerPage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   int _selectedValue = 0;
+  final PanelController _panelController = PanelController();
 
   // 임시로 데이터를 저장하는 Map
   final Map<DateTime, IncomeExpense> _incomeExpenseData = {
@@ -29,6 +30,15 @@ class _LedgerPageState extends State<LedgerPage> {
         date: DateTime.utc(2024, 8, 15), income: 10000, expense: 8000),
   };
 
+  initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      if (_panelController.isAttached) {
+        _panelController.hide();
+      }
+    });
+  }
+
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     if (!isSameDay(_selectedDay, selectedDay)) {
       setState(() {
@@ -36,26 +46,52 @@ class _LedgerPageState extends State<LedgerPage> {
         _focusedDay = focusedDay;
       });
       print('🗓 Selected Day: $selectedDay');
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (_panelController.isAttached) {
+          _panelController.show();
+        }
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        color: backgroundBlue,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16.0),
-            _buildTableCalendar(),
-            const SizedBox(height: 40.0),
-            _buildTableLedger(),
-            _buildLineChart(),
-            const SizedBox(height: 40.0),
-            _buildPieChart(),
-          ],
-        ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GestureDetector(
+            onTap: () {
+              if (_panelController.isPanelShown) {
+                _panelController.hide();
+              }
+            },
+            child: SingleChildScrollView(
+              child: Container(
+                color: backgroundBlue,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 16.0),
+                    _buildTableCalendar(),
+                    const SizedBox(height: 40.0),
+                    _buildTableLedger(),
+                    _buildLineChart(),
+                    const SizedBox(height: 40.0),
+                    _buildPieChart(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SlidingUpPanel(
+            controller: _panelController,
+            panel: _buildSlidingUpPanelContent(),
+            minHeight: 330,
+            maxHeight: 1000,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
+          ),
+        ],
       ),
     );
   }
@@ -71,17 +107,20 @@ class _LedgerPageState extends State<LedgerPage> {
               focusedDay: _focusedDay,
               calendarFormat: CalendarFormat.week,
               onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return _buildBottomSheetContent(selectedDay);
-                  },
-                );
+                _onDaySelected(selectedDay, focusedDay);
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                  _panelController.show(); // 패널 열기
+                } else {
+                  if (!_panelController.isPanelShown) {
+                    _panelController.show();
+                  } else {
+                    _panelController.hide();
+                  }
+                }
               },
               locale: 'ko_KR',
               selectedDayPredicate: (day) {
@@ -128,7 +167,7 @@ class _LedgerPageState extends State<LedgerPage> {
                   children: [
                     Text(
                       DateFormat.yMMMM('ko_KR').format(_focusedDay),
-                      style: body1(gray6),
+                      style: body1(Colors.transparent),
                     ),
                   ],
                 ),
@@ -198,15 +237,19 @@ class _LedgerPageState extends State<LedgerPage> {
     });
   }
 
-  Widget _buildBottomSheetContent(DateTime selectedDay) {
-    // selectedDay에서 시간을 제거하고 날짜만 비교하기 위해 사용
+  Widget _buildSlidingUpPanelContent() {
+    if (_selectedDay == null) {
+      return Center(
+        child: Text("선택된 날짜가 없습니다.", style: body1(Colors.black)),
+      );
+    }
+
     final DateTime selectedDateOnly = DateTime(
-      selectedDay.year,
-      selectedDay.month,
-      selectedDay.day,
+      _selectedDay!.year,
+      _selectedDay!.month,
+      _selectedDay!.day,
     );
 
-    // _incomeExpenseData에서 selectedDateOnly에 해당하는 데이터 찾기
     final incomeExpense = _incomeExpenseData.keys.firstWhere(
       (date) =>
           date.year == selectedDateOnly.year &&
@@ -218,74 +261,167 @@ class _LedgerPageState extends State<LedgerPage> {
     final hasData = incomeExpense != DateTime(0);
 
     return Container(
-      padding: const EdgeInsets.only(left: 18.0, right: 18, bottom: 40),
-      height: 340,
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Spacer(),
-          Center(
-            child: !hasData
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Column(children: [
-                          Image.asset('assets/icons/ledgerIcon.png',
-                              width: 130),
-                          Text("오늘의 수입과 지출을 기록하세요!", style: body1(textBlack)),
-                        ]),
-                      ),
-                      const SizedBox(height: 80),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => AddLedgerPage(
-                                  selectedDate: selectedDay.toLocal(),
-                                ),
-                              ),
-                            )
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: primaryBlue500,
-                            foregroundColor: Colors.white,
-                            padding: const EdgeInsets.symmetric(vertical: 12),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          child: Text('장부 추가', style: header4(Colors.white)),
+      padding: const EdgeInsets.only(left: 18.0, right: 50, bottom: 40),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: !hasData
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 80),
+                        Center(
+                          child: Column(children: [
+                            Image.asset('assets/icons/ledgerIcon.png',
+                                width: 130),
+                            Text("오늘의 수입과 지출을 기록하세요!", style: body1(textBlack)),
+                          ]),
                         ),
-                      )
-                    ],
-                  )
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '${DateFormat.yMMMMd('ko_KR').format(selectedDay.toLocal())}',
-                        style: TextStyle(fontSize: 18, color: Colors.black),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '수입: ${_incomeExpenseData[incomeExpense]?.income.toString()}원',
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                      ),
-                      Text(
-                        '지출: ${_incomeExpenseData[incomeExpense]?.expense.toString()}원',
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                      ),
-                    ],
-                  ),
-          ),
-        ],
+                        const SizedBox(height: 80),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () => {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AddLedgerPage(
+                                    selectedDate: _selectedDay!.toLocal(),
+                                  ),
+                                ),
+                              )
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: primaryBlue500,
+                              foregroundColor: Colors.white,
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            child: Text('장부 추가', style: header4(Colors.white)),
+                          ),
+                        )
+                      ],
+                    )
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                            '${DateFormat('yyyy.MM.dd').format(_selectedDay!.toLocal())}',
+                            style: header3B(gray8)),
+                        SizedBox(height: 12),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 33,
+                          child: ElevatedButton(
+                            onPressed: () => {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AddLedgerPage(
+                                    selectedDate: _selectedDay!.toLocal(),
+                                  ),
+                                ),
+                              )
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                side: BorderSide(color: primaryBlue500),
+                              ),
+                            ),
+                            child:
+                                Text('자세히 보기', style: header4(primaryBlue500)),
+                          ),
+                        ),
+                        SizedBox(height: 12),
+                        Row(
+                          children: [
+                            Text('매출', style: body1(gray4)),
+                            const SizedBox(width: 12),
+                            Text(
+                                '${_incomeExpenseData[selectedDateOnly]?..toString()}원',
+                                style: header4(primaryBlue300))
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Table(
+                          columnWidths: {
+                            0: FlexColumnWidth(3),
+                            1: FlexColumnWidth(2),
+                            2: FlexColumnWidth(1),
+                            3: FlexColumnWidth(3),
+                          },
+                          children: [
+                            TableRow(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child: Text('어획량', style: caption1(gray4)),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child:
+                                      Text('단가 (1kg)', style: caption1(gray4)),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child: Text(''),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child: Text('총합', style: caption1(gray4)),
+                                ),
+                              ],
+                            ),
+                            _buildTableRow('우럭 120kg', '2,000', '240,000원'),
+                            _buildTableRow('도다리 183kg', '988', '642,000원'),
+                            _buildTableRow('아귀 133kg', '1,230', '231,070원'),
+                          ],
+                        ),
+                        Divider(color: gray1, thickness: 1, endIndent: 156),
+                        Row(
+                          children: [
+                            Text('지출', style: body1(gray4)),
+                            const SizedBox(width: 12),
+                            Text(
+                                '${_incomeExpenseData[selectedDateOnly]?.expense.toString()}원',
+                                style: header4(primaryYellow900))
+                          ],
+                        ),
+                      ],
+                    ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  TableRow _buildTableRow(String item, String price, String total) {
+    return TableRow(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Text(item, style: body2(black)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Text('× $price', style: body2(black)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Text('=', style: body2(black)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Text(total, style: body2(black)),
+        ),
+      ],
     );
   }
 
