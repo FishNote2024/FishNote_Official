6a947af08350023f7ad1c4f08715d01878ee3843
haram22 <kimhr9889@handong.ac.kr>
2024-08-23 16:55:36 +0900

    [Feat] 마이페이지 홈화면 연결

--- lib/net/view/net_tab_view.dart
+++ lib/net/view/net_tab_view.dart
@@ -1,4 +1,5 @@
-import 'package:fish_note/net/view/get_net/get_net_page.dart';
+import 'package:fish_note/net/view/get_net/after_get_net_page.dart';
+import 'package:fish_note/net/view/get_net/before_get_net_page.dart';
 import 'package:fish_note/net/view/throw_net/throw_net_page.dart';
 import 'package:flutter/material.dart';
 import 'package:fish_note/theme/colors.dart';
@@ -58,7 +59,7 @@ class _NetTabBarViewState extends State<NetTabBarView>
         padding: const EdgeInsets.all(15.0),
         child: TabBarView(
           controller: _tabController,
-          children: [GetNetPage(), ThrowNetPage()],
+          children: [BeforeGetNetPage(), AfterGetNetPage()],
         ),
       ),
     );
