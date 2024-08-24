b84f0227fcbd8bb705a0812ac01aa68bdf8eb10f
haram22 <kimhr9889@handong.ac.kr>
2024-08-24 18:38:12 +0900

    [Feat] 메모 텍스트필드 키보드 올라올 시 overflow 해결

--- lib/net/view/get_net/get_net_note.dart
+++ lib/net/view/get_net/get_net_note.dart
@@ -15,6 +15,7 @@ class _GetNetNoteState extends State<GetNetNote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ElevatedButton(
@@ -31,43 +32,45 @@ class _GetNetNoteState extends State<GetNetNote> {
           child: Text('양망완료', style: header1B(Colors.white)),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 16),
-            Text('메모를 추가해보세요', style: header1B()),
-            const SizedBox(height: 8),
-            Text(
-              '오늘 어획에 있어 기록하고 싶은 특징이 있다면\n자유롭게 기록해보세요.',
-              style: body1(gray6),
-            ),
-            const SizedBox(height: 32),
-            SizedBox(
-              height: 418,
-              child: TextField(
-                onChanged: (value) {
-                  setState(() {
-                    selectedList = [value];
-                  });
-                },
-                maxLines: 35,
-                decoration: InputDecoration(
-                  hintText: '이곳에 메모를 입력해주세요.\n메모는 스킵 가능합니다.',
-                  hintStyle: body1(gray6),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: gray2),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(color: primaryBlue500),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 16),
+              Text('메모를 추가해보세요', style: header1B()),
+              const SizedBox(height: 8),
+              Text(
+                '오늘 어획에 있어 기록하고 싶은 특징이 있다면\n자유롭게 기록해보세요.',
+                style: body1(gray6),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                height: 418,
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      selectedList = [value];
+                    });
+                  },
+                  maxLines: 35,
+                  decoration: InputDecoration(
+                    hintText: '이곳에 메모를 입력해주세요.\n메모는 스킵 가능합니다.',
+                    hintStyle: body1(gray6),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: gray2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(color: primaryBlue500),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
