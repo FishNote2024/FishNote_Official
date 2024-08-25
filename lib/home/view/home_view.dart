b91d362cdea24c42e22494255c63ba37c38d3cb4
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-08-25 14:38:22 +0900

    feat : 날씨 디테일 UI 완료 + 스크롤 시간 움직이기 완료

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,9 +1,11 @@
 import 'package:fish_note/home/view/net_wait_card.dart';
 import 'package:fish_note/home/view/vertical_outlined_button.dart';
+import 'package:fish_note/home/view/weather/weather_detail_view.dart';
 import 'package:fish_note/theme/colors.dart';
 import 'package:fish_note/theme/font.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fish_note/home/model/weatherAPITimeSync.dart';
 
 import '../model/weather_api.dart';
 
@@ -17,25 +19,66 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with TickerProviderStateMixin {
   late final TabController _tabController;
   late Future<Map<String, dynamic>> weatherData;
+  late Map<String, dynamic> data;
+  final ScrollController _scrollController = ScrollController();
+  bool _hasJumped = false;
+  int differenceInMinutes = 0;
+
+  final DateTime _initialTime = DateTime.now();
+  DateTime _currentTime = DateTime.now();
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
     ApiService apiService = ApiService();
-    weatherData = apiService.fetchData(nx: 36.190, ny: 129.358);
+
+    WeatherAPITimeSync closestForecastTime = WeatherAPITimeSync();
+    String closestTime = closestForecastTime.getClosestTime(DateTime
+        .now()); //'0200', '0500', '0800', '1100', '1400', '1700', '2000', '2300'
+    String formattedDate =
+        closestForecastTime.getFormattedDate(DateTime.now()); //'20240809'
+    weatherData = apiService.fetchData(
+        nx: 36.190,
+        ny: 129.358,
+        closestTime: closestTime,
+        formattedDate: formattedDate);
+
+// 날짜 문자열을 DateTime 객체로 변환
+    int year = int.parse(formattedDate.substring(0, 4));
+    int month = int.parse(formattedDate.substring(4, 6));
+    int day = int.parse(formattedDate.substring(6, 8));
+
+// 시간 문자열을 시간과 분으로 분리하여 정수로 변환
+    int hour = int.parse(closestTime.substring(0, 2));
+    int minute = int.parse(closestTime.substring(2, 4));
+
+// DateTime 객체 생성
+    DateTime dateTime = DateTime(year, month, day, hour, minute);
+
+    Duration difference = _initialTime.difference(dateTime);
+    differenceInMinutes = difference.inMinutes;
+
+    _scrollController.addListener(() {
+      setState(() {
+        double scrollPosition =
+            _scrollController.position.pixels - differenceInMinutes + 88;
+        _currentTime =
+            _initialTime.add(Duration(minutes: (scrollPosition / 1).round()));
+      });
+    });
   }
 
   @override
   void dispose() {
     _tabController.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     DateTime currentDate = DateTime.now();
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -104,52 +147,148 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
                 ),
               ),
               const SizedBox(height: 16.0),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                    border: Border.all(color: gray2)),
-                child: FutureBuilder<Map<String, dynamic>>(
-                    future: weatherData,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      } else if (snapshot.hasError) {
-                        return Center(child: Text('Error: ${snapshot.error}'));
-                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                        return const Center(child: Text('No data available'));
-                      }
+              Stack(children: [
+                GestureDetector(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: gray2)),
+                    child: FutureBuilder<Map<String, dynamic>>(
+                        future: weatherData,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text('Error: ${snapshot.error}'));
+                          } else if (!snapshot.hasData ||
+                              snapshot.data!.isEmpty) {
+                            return const Center(
+                                child: Text('No data available'));
+                          }
 
-                      Map<String, dynamic> data = snapshot.data!;
+                          data = snapshot.data!;
 
-                      return SingleChildScrollView(
-                        scrollDirection: Axis.horizontal, // 가로 스크롤
-                        child: Row(
-                          children: data.entries.map((entry) {
-                            String time =
-                                "${entry.key.substring(8, 10)}:${entry.key.substring(10, 12)}";
-                            Map<String, dynamic> weatherInfo = entry.value;
+                          if (!_hasJumped) {
+                            WidgetsBinding.instance.addPostFrameCallback((_) {
+                              _scrollController
+                                  .jumpTo(differenceInMinutes.toDouble() - 88);
+                            });
+                            _hasJumped = true;
+                          }
 
-                            String direction = _convertVecToDirection(
-                                int.parse(weatherInfo['VEC']));
-                            IconData icon =
-                                _getWeatherIcon(int.parse(weatherInfo['SKY']));
+                          return Container(
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal, // 가로 스크롤
+                              controller: _scrollController,
+                              child: Row(
+                                children: [
+                                  // 제목 Row 추가
+                                  Column(
+                                    children: [
+                                      Text('시간', style: caption2(gray4)),
+                                      SizedBox(height: 13.0),
+                                      Text('날씨', style: caption2(gray4)),
+                                      SizedBox(height: 13.0),
+                                      Text('풍속', style: caption2(gray4)),
+                                      SizedBox(height: 13.0),
+                                      Text('풍향', style: caption2(gray4)),
+                                      SizedBox(height: 13.0),
+                                      Text('파고', style: caption2(gray4)),
+                                    ],
+                                  ),
+                                  SizedBox(width: 8),
+                                  // const SizedBox(width: 8.0), // 제목과 데이터 간의 간격 추가
+                                  SizedBox(
+                                      width: 1,
+                                      child: Divider(
+                                          color: gray1,
+                                          height: 158,
+                                          thickness: 300)),
+                                  const SizedBox(width: 8.0),
+                                  Row(
+                                    children: data.entries.map((entry) {
+                                      String time =
+                                          "${entry.key.substring(8, 10)}:${entry.key.substring(10, 12)}";
+                                      Map<String, dynamic> weatherInfo =
+                                          entry.value;
 
-                            return Padding(
-                              padding: const EdgeInsets.all(14.0),
-                              child: weatherColumn(
-                                time,
-                                icon,
-                                '${weatherInfo['WSD']}m/s',
-                                direction,
-                                '${weatherInfo['WAV']}m',
+                                      String direction = _convertVecToDirection(
+                                          int.parse(weatherInfo['VEC']));
+                                      IconData icon = _getWeatherIcon(
+                                          int.parse(weatherInfo['SKY']));
+
+                                      return Padding(
+                                        padding: const EdgeInsets.all(0.0),
+                                        child: weatherColumn(
+                                          time,
+                                          icon,
+                                          '${weatherInfo['WSD']}m/s',
+                                          direction,
+                                          '${weatherInfo['WAV']}m',
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ],
                               ),
-                            );
-                          }).toList(),
+                            ),
+                          );
+                        }),
+                  ),
+                  onTap: () {
+                    // Navigator를 통해 다른 페이지로 데이터를 전달하며 이동
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WeatherDetailView(data: data, differenceInMinutes: differenceInMinutes),
+                      ),
+                    );
+                  },
+                ),
+                FutureBuilder<Map<String, dynamic>>(
+                  future: weatherData,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done &&
+                        snapshot.hasData) {
+                      return const Positioned(
+                        left: 65,
+                        top: 0,
+                        bottom: 0,
+                        child: VerticalDivider(
+                          indent: 24,
+                          color: primaryBlue500,
+                          thickness: 2,
+                          width: 20,
                         ),
                       );
-                    }),
+                    } else {
+                      return Container(); // 데이터를 불러오기 전에는 아무것도 그리지 않음
+                    }
+                  },
+                ),
+              ]),
+              Padding(
+                padding: const EdgeInsets.only(left: 28),
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 6.0), // 텍스트 주위의 여백 설정
+                  decoration: BoxDecoration(
+                    color: primaryBlue500, // 배경 색상
+                    borderRadius: BorderRadius.circular(20.0), // 둥근 모서리 설정
+                  ),
+                  child: Text(
+                    '현재 ${_currentTime.hour.toString().padLeft(2,'0')}:${_currentTime.minute.toString().padLeft(2, '0')}',
+                    style: TextStyle(
+                      color: Colors.white, // 텍스트 색상 설정
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
               const SizedBox(height: 16.0),
               Row(children: [
@@ -214,14 +353,32 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   Widget weatherColumn(String time, IconData icon, String windSpeed,
       String direction, String waveHeight) {
-    return Column(
-      children: [
-        Text(time),
-        Icon(icon),
-        Text(windSpeed),
-        Text(direction),
-        Text(waveHeight),
-      ],
+    return SizedBox(
+      width: 60,
+      child: Column(
+        children: [
+          Text(time),
+          const SizedBox(height: 8),
+          Icon(icon),
+          const SizedBox(height: 8),
+          Text(windSpeed),
+          const SizedBox(height: 8),
+          Text(direction),
+          const SizedBox(height: 8),
+          Text(waveHeight),
+          Padding(
+            padding: const EdgeInsets.only(left: 59),
+            child: SizedBox(
+              width: 1,
+              child: Divider(
+                color: gray1,
+                height: 1,
+                thickness: 140,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
---
+++ lib/home/view/weather/weather_detail_view.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../theme/colors.dart';
+import '../../../theme/font.dart';
+
+class WeatherDetailView extends StatefulWidget {
+  final Map<String, dynamic> data;
+  final int differenceInMinutes;
+
+  WeatherDetailView(
+      {super.key, required this.data, required this.differenceInMinutes});
+
+  @override
+  _WeatherDetailViewState createState() => _WeatherDetailViewState();
+}
+
+class _WeatherDetailViewState extends State<WeatherDetailView> {
+  final ScrollController _scrollController = ScrollController();
+  late DateTime _currentTime;
+  late DateTime _initialTime;
+  bool _hasJumped = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialTime = DateTime.now();
+    _currentTime = _initialTime;
+
+    _scrollController.addListener(() {
+      setState(() {
+        double scrollPosition =
+            _scrollController.position.pixels - widget.differenceInMinutes + 46;
+        _currentTime =
+            _initialTime.add(Duration(minutes: (scrollPosition / 1).round()));
+      });
+    });
+    if (!_hasJumped) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollController.jumpTo(widget.differenceInMinutes.toDouble() - 46);
+      });
+      _hasJumped = true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, size: 15, color: gray7),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.location_on, size: 24, color: primaryBlue500),
+            Text("서해바다"),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 40,
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: alertRedDefault,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset('assets/icons/noti.svg'),
+                  const SizedBox(width: 8.0),
+                  Expanded(
+                    child: Text(
+                      '기상특보 6.28 18:00 서해 앞바다 풍랑주의보',
+                      style: body3(Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Stack(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(color: gray2)),
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.only(left: 16, top: 16),
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "날씨 및 해양정보",
+                            style: caption1(gray8),
+                            textAlign: TextAlign.left,
+                          )),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal, // 가로 스크롤
+                        controller: _scrollController,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 16),
+                          child: Row(
+                            children: [
+                              // 제목 Row 추가
+                              Column(
+                                children: [
+                                  Text('시간', style: caption2(gray4)),
+                                  const SizedBox(height: 13.0),
+                                  Text('날씨', style: caption2(gray4)),
+                                  const SizedBox(height: 13.0),
+                                  Text('온도', style: caption2(gray4)),
+                                  const SizedBox(height: 7.0),
+                                  Text('강수\n확률', style: caption2(gray4)),
+                                  const SizedBox(height: 7.0),
+                                  Text('풍속', style: caption2(gray4)),
+                                  const SizedBox(height: 13.0),
+                                  Text('풍향', style: caption2(gray4)),
+                                  const SizedBox(height: 13.0),
+                                  Text('강수량', style: caption2(gray4)),
+                                  const SizedBox(height: 13.0),
+                                  Text('파고', style: caption2(gray4)),
+                                ],
+                              ),
+                              const SizedBox(width: 8),
+                              SizedBox(
+                                width: 1,
+                                child: Divider(
+                                  color: gray1,
+                                  height: 158,
+                                  thickness: 300,
+                                ),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Row(
+                                children: widget.data.entries.map((entry) {
+                                  String time =
+                                      "${entry.key.substring(8, 10)}:${entry.key.substring(10, 12)}";
+                                  Map<String, dynamic> weatherInfo =
+                                      entry.value;
+                                  String rain = (weatherInfo['PCP'] == "강수없음")
+                                      ? "0mm"
+                                      : weatherInfo['PCP'];
+
+                                  String direction = _convertVecToDirection(
+                                      int.parse(weatherInfo['VEC']));
+                                  IconData icon = _getWeatherIcon(
+                                      int.parse(weatherInfo['SKY']));
+
+                                  return Padding(
+                                    padding: const EdgeInsets.all(0.0),
+                                    child: weatherColumn(
+                                        time,
+                                        icon,
+                                        '${weatherInfo['TMP']}°C',
+                                        '${weatherInfo['POP']}%',
+                                        '${weatherInfo['WSD']}m/s',
+                                        direction,
+                                        '${rain}',
+                                        '${weatherInfo['WAV']}m'),
+                                  );
+                                }).toList(),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 65,
+                top: 0,
+                bottom: 0,
+                child: Container(
+                  height: 10,
+                  child: VerticalDivider(
+                    indent: 70,
+                    color: primaryBlue500,
+                    thickness: 2,
+                    width: 20,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 28),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0, vertical: 6.0),
+                    decoration: BoxDecoration(
+                      color: primaryBlue500,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Text(
+                      '현재 ${_currentTime.hour.toString().padLeft(2, '0')}:${_currentTime.minute.toString().padLeft(2, '0')}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _convertVecToDirection(int vec) {
+    if (vec >= 0 && vec < 45) return '↑';
+    if (vec >= 45 && vec < 135) return '→';
+    if (vec >= 135 && vec < 225) return '↓';
+    if (vec >= 225 && vec < 315) return '←';
+    return '↑';
+  }
+
+  IconData _getWeatherIcon(int sky) {
+    switch (sky) {
+      case 1:
+        return Icons.wb_sunny;
+      case 2:
+        return Icons.cloud;
+      case 3:
+        return Icons.cloud_queue;
+      case 4:
+        return Icons.grain; // 비 또는 눈 아이콘으로 변경 가능
+      default:
+        return Icons.wb_sunny;
+    }
+  }
+
+  Widget weatherColumn(
+      String time,
+      IconData icon,
+      String temp,
+      String rainPercent,
+      String windSpeed,
+      String direction,
+      String rain,
+      String waveHeight) {
+    return SizedBox(
+      width: 60,
+      child: Column(
+        children: [
+          Text(time),
+          const SizedBox(height: 8),
+          Icon(icon),
+          const SizedBox(height: 8),
+          Text(temp),
+          const SizedBox(height: 8),
+          Text(rainPercent),
+          const SizedBox(height: 8),
+          Text(windSpeed),
+          const SizedBox(height: 8),
+          Text(direction),
+          const SizedBox(height: 8),
+          Text(rain),
+          const SizedBox(height: 8),
+          Text(waveHeight),
+        ],
+      ),
+    );
+  }
+}
