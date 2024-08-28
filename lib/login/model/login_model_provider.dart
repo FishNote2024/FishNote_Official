2eb9b2169f3d4a848b34359369f382c7db01ff66
eunoia-jaxson <eunoia@handong.ac.kr>
2024-08-29 02:08:14 +0900

    Feat : 최초 로그인 시 회원가입 페이지 이동

--- lib/login/model/login_model_provider.dart
+++ lib/login/model/login_model_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class LoginModelProvider with ChangeNotifier {
@@ -8,6 +9,8 @@ class LoginModelProvider with ChangeNotifier {
   String get name => _name;
   String get kakaoId => _kakaoId;
 
+  final db = FirebaseFirestore.instance;
+
   // Setters with notifyListeners to update UI when data changes
   void setName(String name) {
     _name = name;
@@ -16,6 +19,15 @@ class LoginModelProvider with ChangeNotifier {
 
   void setKakaoId(String kakaoId) {
     _kakaoId = kakaoId;
+    notifyListeners();
+  }
+
+  // Save name to Firestore
+  void saveName() async {
+    final docRef = db.collection("users").doc(_kakaoId);
+    await docRef.set({
+      'name': _name,
+    }, SetOptions(merge: true));
     notifyListeners();
   }
 }
